bfaeeb79c0fca3d91b144a0c7d0349d71c39a250
jacy <johnsoncr.hong@gmail.com>
2015-07-20 11:48:36 +0800

    count coins problem: https://www.hackerrank.com/challenges/coin-change

---
+++ src/coins.erl
@@ -0,0 +1,24 @@
+-module(coins).
+-compile(export_all). 
+
+count(N, L) ->
+    print(count(N, L, [])).
+
+count(0, _, Ways) ->
+    Ways;
+count(N, _, _) when N < 0 ->
+    [];
+count(_, [], _) ->
+    [];
+count(N, [H | T]=Coins, Ways) ->
+    WithFirstCoin = count(N - H, Coins, [H | Ways]),
+	WithOutFirstCoin = count(N, T, Ways),
+	[WithFirstCoin,WithOutFirstCoin].
+
+print([]) ->
+	ok;
+print([H | T] = L) ->
+	case is_list(H) of
+		false -> io:format("~w~n",[L]);
+		true -> print(H), print(T)
+	end.
