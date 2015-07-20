da564ef03962a783d6dc048fa8adcadc7e5c2690
jacy <331308291@qq.com>
2015-07-20 11:59:05 +0800

    Update coins.erl

--- src/coins.erl
+++ src/coins.erl
@@ -2,16 +2,16 @@
 -compile(export_all). 
 
 count(N, L) ->
-    print(count(N, L, [])).
+print(count(N, L, [])).
 
 count(0, _, Ways) ->
-    Ways;
+	Ways;
 count(N, _, _) when N < 0 ->
-    [];
+	[];
 count(_, [], _) ->
-    [];
+	[];
 count(N, [H | T]=Coins, Ways) ->
-    WithFirstCoin = count(N - H, Coins, [H | Ways]),
+	WithFirstCoin = count(N - H, Coins, [H | Ways]),
 	WithOutFirstCoin = count(N, T, Ways),
 	[WithFirstCoin,WithOutFirstCoin].
 
