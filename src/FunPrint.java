b11249ddb2089c862d635e9c5a67a112e07f8707
jacy <331308291@qq.com>
2015-08-27 13:23:17 +0800

    Update FunPrint.java

--- src/FunPrint.java
+++ src/FunPrint.java
@@ -1,3 +1,6 @@
+/* Given a two-dmension array of N * N elements(N rows, N columns), write a function that prints the elements in the array 
+in diagonal strips from top right to bottom left. */
+
 public calss FunPrint{
 	public static void main(String[] args) {
 		Integer[][] source = new Integer[][] { { 1, 2, 3, 4, 5 }, { 6, 7, 8, 9, 10 }, { 11, 12, 13, 14, 15 }, { 16, 17, 18, 19, 20 }, { 21, 22, 23, 24, 25 } };
