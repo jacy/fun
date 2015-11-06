672e891426f462fce2e361f26d6d3ca7170c7cad
jacy <331308291@qq.com>
2015-11-06 17:12:12 +0800

    Update bits.erl

--- src/bits.erl
+++ src/bits.erl
@@ -1,14 +1,7 @@
 -module(bits).
 -compile(export_all). 
 
-win_count(Buy, Result) ->
-	case is_win(Buy, Result) of
-		true -> 
-			combination(length(Buy) - 5 ,2);
-		false -> 
-			0
-	end.
-	
+
 int_to_bit(Num) ->
 	1 bsl Num.
 
@@ -24,6 +17,14 @@ is_win(Buy, Result) ->
 	ResultBits = ints_bits(Result),
 	(BuyBits band ResultBits) == ResultBits.
 
+win_count(Buy, Result) ->
+	case is_win(Buy, Result) of
+		true -> 
+			combination(length(Buy) - 5 ,2);
+		false -> 
+		0
+	end.
+
 factorial(N) -> factorial(1, N, 1).
 factorial(M, N, R) when M =< N ->
   factorial(M+1, N, R*M);
@@ -34,3 +35,34 @@ permutation(N,R) ->
 
 combination(N, R) ->
   permutation(N, R) div factorial(R).
+  
+permute([]) -> 
+	[[]];
+permute(L) -> 
+	[[X|Y] || X<-L, Y<-permute(L--[X])].
+
+combination_seq(0,_) ->
+    [[]];
+combination_seq(_,[]) ->
+    [];
+combination_seq(N,[H|T]) ->
+    [[H|L] || L <- combination_seq(N-1,T)]++combination_seq(N,T).
+    
+win_count_test() ->
+	Start = erlang:timestamp(),
+	Combs = combination_seq(5, lists:seq(1,11)),
+	print_comb(Combs),
+	End = erlang:timestamp(),
+	timer:now_diff(End,Start).
+	
+print_comb([]) ->
+	ok;
+print_comb([H|L]) ->
+	print_perm(permute(H)),
+	print_comb(L).
+print_perm([]) ->
+	ok;
+print_perm([H|L]) ->
+	Buy = lists:seq(1,8),
+	io:format("b~p,r:~p,c:~p~n",[Buy, H, win_count(Buy,H)]),
+	print_perm(L).
